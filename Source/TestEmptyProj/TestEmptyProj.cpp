bb8fc0affb2a334e4d1badca598a8392c84c12db
AbdElRahman Yousry <ar.yousry@gmail.com>
2023-07-06 11:33:01 +0300

    2

---
+++ Source/TestEmptyProj/TestEmptyProj.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestEmptyProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestEmptyProj, "TestEmptyProj" );
